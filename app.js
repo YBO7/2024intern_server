2739ec209bad7e6e482ed75b079c9c4bdecd0cd1
Yubin Oh <sale506559@gmail.com>
2024-11-18 15:50:22 +0900

    fix:1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 3001;
+
+app.use(cors());
+
+app.get("/strawberry", (req, res) => {
+    res.send("STB");
+});
+
+app.get("/apple", (req, res) => {
+    res.send("APPL");
+});
+
+app.listen(port, () => {
+    console.log("connected on server port" + port);
+});
+
+module.exports = app;
+
